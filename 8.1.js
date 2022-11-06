d59e240d356b595c547c54fddc8fc518f6031e80
Fufulele <112864040+Fufulele@users.noreply.github.com>
2022-11-06 18:29:43 +0100

    Add files via upload

---
+++ 8.1.js
@@ -0,0 +1,59 @@
+function multiply() {
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    if (getal1 > 0  && getal2 > 0) {
+        var antwoord = getal1 * getal2;
+        document.getElementById("antwoord").innerHTML = "antwoord is: " + antwoord;
+    }
+
+ else {
+    document.getElementById("antwoord").innerHTML = "getal is te laag";
+}
+}
+
+function multiply() {
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    if (getal1 > 0  && getal2 > 0) {
+        var antwoord = getal1 * getal2;
+        document.getElementById("antwoord").innerHTML = "Het antwoord is: " + antwoord;
+    } 
+    else {
+        document.getElementById("antwoord").innerHTML = "Het getal is te laag";
+    }
+}
+function divide() {
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    if (getal1 > 0  && getal2 > 0) {
+        var antwoord = getal1 / getal2;
+        document.getElementById("antwoord").innerHTML = "Het antwoord is: " + antwoord;
+    } 
+    else {
+        document.getElementById("antwoord").innerHTML = "Het getal is te laag";
+    }
+}
+
+function add() {
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    if (getal1 > 0  && getal2 > 0) {
+        var antwoord = +getal1 + +getal2;
+        document.getElementById("antwoord").innerHTML = "Uitkomst: " + antwoord;
+    } 
+    else {
+        document.getElementById("antwoord").innerHTML = "te laag getal";
+    }
+}
+
+function minus() {
+    var getal1 = document.getElementById("getal1").value;
+    var getal2 = document.getElementById("getal2").value;
+    if (getal1 > 0  && getal2 > 0) {
+        var antwoord = getal1 - getal2;
+        document.getElementById("antwoord").innerHTML = "uitkomst: " + antwoord;
+    } 
+    else {
+        document.getElementById("antwoord").innerHTML = "te laag getal";
+    }
+}
